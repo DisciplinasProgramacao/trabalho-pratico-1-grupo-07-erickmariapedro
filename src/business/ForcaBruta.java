a3e6a78ad8dfa9cfe250cd7469ea863cdc6f7270
Maria Clara <mariaclarasales8@gmail.com>
2022-05-12 17:32:50 -0300

    Formatação

--- src/business/ForcaBruta.java
+++ src/business/ForcaBruta.java
@@ -5,15 +5,15 @@ import java.util.Collections;
 import java.util.List;
 
 public class ForcaBruta {
-	
+
 	public static List<List<ItemMochila>> encontrarSubConjunto(List<ItemMochila> itens) {
 
 		List<List<ItemMochila>> subConjuntos = new ArrayList<>();
 
 		if (itens.isEmpty()) {
-            subConjuntos.add(Collections.emptyList());
-            return subConjuntos;		
-            } else {
+			subConjuntos.add(Collections.emptyList());
+			return subConjuntos;
+		} else {
 			List<List<ItemMochila>> recursivoConjunto = encontrarSubConjunto(itens.subList(1, itens.size()));
 			for (int i = 0; i < recursivoConjunto.size(); i++) {
 				List<ItemMochila> conjunto = recursivoConjunto.get(i);
@@ -25,28 +25,28 @@ public class ForcaBruta {
 		}
 		return subConjuntos;
 	}
-	
+
 	public static int conjuntoMaisValioso(List<ItemMochila> itens, int capacidade) {
 		List<List<ItemMochila>> subConjuntos = encontrarSubConjunto(itens);
-		int maisValioso = 0; 
-		
-		for(int i = 0; i < subConjuntos.size(); i++) {
+		int maisValioso = 0;
+
+		for (int i = 0; i < subConjuntos.size(); i++) {
 			double peso = 0;
 			int valor = 0;
-	        for (ItemMochila item : subConjuntos.get(i)) {
-	            peso += item.getPeso();
-	        }
-	                
-			if(peso <= capacidade ) {
-		        for (ItemMochila item : subConjuntos.get(i)) {
-		            valor += item.getValor();
-		        }
-		        if(valor > maisValioso ) {
-		        	maisValioso = valor; 
-		        }
+			for (ItemMochila item : subConjuntos.get(i)) {
+				peso += item.getPeso();
+			}
+
+			if (peso <= capacidade) {
+				for (ItemMochila item : subConjuntos.get(i)) {
+					valor += item.getValor();
+				}
+				if (valor > maisValioso) {
+					maisValioso = valor;
+				}
 			}
 		}
-        return maisValioso;
+		return maisValioso;
 	}
 
 }
