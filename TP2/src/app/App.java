e72fd8602c36a5f51f7c69f3a3861990b29640b1
Maria Clara <mariaclarasales8@gmail.com>
2022-06-25 15:12:14 -0300

    tempo de excussão

--- TP2/src/app/App.java
+++ TP2/src/app/App.java
@@ -10,20 +10,47 @@ public class App {
     public static void main(String[] args) {
         MatrizDAO leitor = new MatrizDAO();
         int[][] matriz = leitor.get("LaminacaoTeste4.txt");
-
+        long tempoExecucaoInicial = 0;
+        long tempoExecucaoFinal = 0;
+        long tempoExecucaoBacktracking = 0;
+        long tempoExecucaoProgDinamica = 0;
+        long tempoExecucaoGuloso = 0;
+             
         Backtracking backtracking = new Backtracking();
-        ProgDinamica progDinamica = new ProgDinamica(matriz);
-        Guloso guloso = new Guloso();
+        Guloso guloso = new Guloso();    
         Rolo[][] matrizBacktracking = backtracking.construirMatrizRolos(matriz);
-        System.out.println("(Backtracking) Melhor custo: " + backtracking.backtracking(matrizBacktracking,
+
+        System.out.println("Resultados Backtracking");
+
+        tempoExecucaoInicial = System.currentTimeMillis();
+        System.out.println("Melhor custo: " + backtracking.backtracking(matrizBacktracking,
                 0,
                 matriz[0].length - 1,
                 matriz[0][0],
                 0,
                 Double.POSITIVE_INFINITY
-        ));
+        ));        tempoExecucaoFinal = System.currentTimeMillis();
+        tempoExecucaoBacktracking = ((tempoExecucaoFinal - tempoExecucaoInicial));
 
-        System.out.println("(Programação Dinâmica) Sequência de rolos: " +progDinamica.getSequenciaCilindros().toString());
-        System.out.println("(Guloso) Sequência de rolos: " + guloso.guloso(matriz));
+        System.out.println("Tempo de execução: " + tempoExecucaoBacktracking + " ms\n");
+
+        System.out.println("Resultados Programação Dinâmica");
+        
+        tempoExecucaoInicial = System.currentTimeMillis();
+        ProgDinamica progDinamica = new ProgDinamica(matriz);
+        tempoExecucaoFinal = System.currentTimeMillis();
+        tempoExecucaoProgDinamica = ((tempoExecucaoFinal - tempoExecucaoInicial));
+        
+        System.out.println("Sequência de rolos: " + progDinamica.getSequenciaCilindros().toString());
+        System.out.println("Tempo de execução: " + tempoExecucaoProgDinamica + " ms\n");
+
+        System.out.println("Resultados Guloso");
+        
+        tempoExecucaoInicial = System.currentTimeMillis();
+        System.out.println("Sequência de rolos: " + guloso.guloso(matriz));
+        tempoExecucaoFinal = System.currentTimeMillis();
+        tempoExecucaoGuloso = ((tempoExecucaoFinal - tempoExecucaoInicial));
+        
+        System.out.println("Tempo de execução: " + tempoExecucaoGuloso + " ms\n");
     }
 }
